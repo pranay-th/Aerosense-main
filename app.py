e04e3c4c52c6867321edd915c1576897557ecaf6
pranay-th <93934481+pranay-th@users.noreply.github.com>
2024-09-06 15:10:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -235,5 +235,4 @@ def scrape_weather():
     return jsonify({'message': 'Weather data scraped and saved successfully!', 'data': weather_data})
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, port=port)
+    app.run(debug=True, host='0.0.0.0',port=5000)
